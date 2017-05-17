d74ed7e07c88b01f748196f4dfad2bcd72adcf6e
Ondrej Prazak <oprazak@redhat.com>
2017-05-17 15:39:17 +0200

    Fixes #19576 - Fix test failures after minitest upgrade

--- test/functional/api/v2/template_controller_test.rb
+++ test/functional/api/v2/template_controller_test.rb
@@ -4,7 +4,7 @@ module Api
   module V2
     class TemplateControllerTest < ::ActionController::TestCase
       test "should import from git" do
-        post :import, params: { 'repo' => "#{ForemanTemplates::Engine.root}/test/templates/core", 'prefix' => '' }
+        post :import, 'repo' => "#{ForemanTemplates::Engine.root}/test/templates/core", 'prefix' => ''
         assert_response :success
       end
     end
