177b26218e243e160c7083a2c7669621f2df438a
Ondrej Prazak <oprazak@redhat.com>
2019-09-30 14:38:45 +0200

    Refs #16986 - Fix rubocop

--- lib/foreman_templates/engine.rb
+++ lib/foreman_templates/engine.rb
@@ -22,7 +22,7 @@ module ForemanTemplates
 
     initializer 'foreman_templates.register_plugin', :before => :finisher_hook do
       Foreman::Plugin.register :foreman_templates do
-        requires_foreman '>= 1.23'
+        requires_foreman '>= 1.24'
 
         apipie_documented_controllers ["#{ForemanTemplates::Engine.root}/app/controllers/api/v2/*.rb"]
 
