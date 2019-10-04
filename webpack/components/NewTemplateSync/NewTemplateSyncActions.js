ba37295f0f2e5d9b9811b412279c59b74f68c67e
Ondrej Prazak <oprazak@redhat.com>
2019-10-04 12:25:18 +0200

    Fixes #27964 - Reset pagination on sync success

--- webpack/components/NewTemplateSync/NewTemplateSyncActions.js
+++ webpack/components/NewTemplateSync/NewTemplateSyncActions.js
@@ -5,13 +5,20 @@ import {
   SYNC_SETTINGS_REQUEST,
   SYNC_SETTINGS_SUCCESS,
   SYNC_SETTINGS_FAILURE,
+  SYNC_RESULT_PAGINATION_CHANGE,
 } from '../../consts';
+
+import { initialState } from '../TemplateSyncResult/TemplateSyncResultReducer';
 
 export const getSyncSettings = url => async dispatch => {
   dispatch({ type: SYNC_SETTINGS_REQUEST });
 
   try {
     const { data } = await api.get(url);
+    dispatch({
+      type: SYNC_RESULT_PAGINATION_CHANGE,
+      payload: { pagination: initialState.pagination },
+    });
     return dispatch({
       type: SYNC_SETTINGS_SUCCESS,
       payload: {
