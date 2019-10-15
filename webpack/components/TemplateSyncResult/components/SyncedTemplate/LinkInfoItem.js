507b5529847dfa089961e76b9d3965814a4ad567
Ondrej Prazak <oprazak@redhat.com>
2019-10-15 13:28:36 +0200

    Fixes #28044 - Align sync results without link

--- webpack/components/TemplateSyncResult/components/SyncedTemplate/LinkInfoItem.js
+++ webpack/components/TemplateSyncResult/components/SyncedTemplate/LinkInfoItem.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { ListView } from 'patternfly-react';
+import PropTypes from 'prop-types';
 
 import { itemIteratorId } from './helpers';
 
-const LinkInfoItem = ({ template, editPath, attr }) => {
+const wrapWithLink = (template, editPath) => {
   if (template.id && template.canEdit) {
     return (
-      <ListView.InfoItem
-        key={itemIteratorId(template, attr)}
-        className="additional-info-wide"
+      <a
+        href={editPath.replace(':id', template.id)}
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <a
-          href={editPath.replace(':id', template.id)}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {template.name}
-        </a>
-      </ListView.InfoItem>
+        {template.name}
+      </a>
     );
   }
   return template.name || ' ';
+};
+
+const LinkInfoItem = ({ template, editPath, attr }) => (
+  <ListView.InfoItem
+    key={itemIteratorId(template, attr)}
+    className="additional-info-wide"
+  >
+    {wrapWithLink(template, editPath)}
+  </ListView.InfoItem>
+);
+
+LinkInfoItem.propTypes = {
+  template: PropTypes.object.isRequired,
+  editPath: PropTypes.string.isRequired,
+  attr: PropTypes.string.isRequired,
 };
 
 export default LinkInfoItem;
