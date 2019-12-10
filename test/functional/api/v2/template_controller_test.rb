2837babc0be3c86237d5450d76e2e0d339eec6b4
Ondrej Prazak <oprazak@redhat.com>
2019-12-10 09:11:19 +0100

    Fixes #28465 - Make Tier 1 tests pass

--- test/functional/api/v2/template_controller_test.rb
+++ test/functional/api/v2/template_controller_test.rb
@@ -3,9 +3,13 @@ require 'test_plugin_helper'
 module Api
   module V2
     class TemplateControllerTest < ::ActionController::TestCase
+      setup do
+        @org = FactoryBot.create(:organization, :name => 'ForemanTemplatesOrg')
+        @loc = FactoryBot.create(:location, :name => 'ForemanTemplatesLoc')
+      end
 
       test "should import from git" do
-        post :import, params: { 'repo' => "#{ForemanTemplates::Engine.root}/test/templates/core", 'prefix' => '' }
+        post :import, params: { 'repo' => "#{template_fixtures_path}/core", 'prefix' => '' }
         assert_response :success
       end
 
@@ -27,7 +31,7 @@ module Api
       end
 
       test "should import filtered templates" do
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => 'filter1', "prefix" => "filtTemp_", "associate" => "new"}
+        post :import, params: { "repo" => "#{template_fixtures_path}/core", "filter" => 'filter1', "prefix" => "filtTemp_", "associate" => "new" }
         assert_response :success
         templates = JSON.parse(@response.body)['message']['templates']
         templates.each do |template|
@@ -38,79 +42,60 @@ module Api
       end
 
       test "should not import negate filtered templates" do
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => 'filter1', "prefix" => '', "negate" => true }
+        post :import, params: { "repo" => "#{template_fixtures_path}/core", "filter" => 'filter1', "prefix" => '', "negate" => true }
         assert_response :success
         templates = JSON.parse(@response.body)['message']['templates']
         templates.each do |template|
           if template['name'] == 'filter1 template'
             assert_not template['imported']
           end
-        end
-      end
-
-      test "should import from git branch" do
-        post :import, params: { "repo" => 'https://github.com/SatelliteQE/foreman_templates.git', "prefix" => "gitBranch", "branch" => "example", "associate" => "new" }
-        assert_response :success
-        templates = JSON.parse(@response.body)['message']['templates']
-        assert_equal templates.length, 11
-        templates.each do |template|
-          assert template['imported']
-        end
-      end
-
-      test "should import from git repository directory" do
-        post :import, params: { "repo" => 'https://github.com/SatelliteQE/foreman_templates.git', "prefix" => "repodir", "branch" => "example", "associate" => "new", "dirname" => "import" }
-        assert_response :success
-        templates = JSON.parse(@response.body)['message']['templates']
-        assert_equal templates.length, 10
-        templates.each do |template|
-          puts template
-          assert template['imported']
         end
       end
 
       test "should not update locked template on import" do
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => "filter1", "prefix" => '', "associate" => "new", "lock" => true }
-        File.open("#{ForemanTemplates::Engine.root}/test/templates/core/filter1.erb", "a") do |filt|
-          filt << "Appended from test\n"
-        end
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => "filter1", "prefix" => '', "associate" => "new", "lock" => true }
+        import_params = {
+          :repo => "#{template_fixtures_path}/locking/core_initial",
+          :prefix => '',
+          :associate => "new",
+          :lock => true
+        }
+
+        ForemanTemplates::TemplateImporter.new(import_params).import!
+        post :import, params: import_params.merge(:repo => "#{template_fixtures_path}/locking/core_updated")
         assert_response :success
-        uptemplates = JSON.parse(@response.body)['message']['templates']
-        uptemplates.each do |template|
-          if template['name'] == 'filter1 template'
-            assert_not template['imported']
-            assert template['changed']
-          end
+        templates = JSON.parse(@response.body)['message']['templates']
+        templates.each do |template|
+          assert template
+          assert_not template['imported']
+          assert template['changed']
+          assert_equal "This template is locked. Please clone it to a new template to customize.", template['validation_errors']['base']
         end
       end
 
       test "should force update locked template on import" do
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => "filter1", "prefix" => '', "associate" => "new", "lock" => true }
-        File.open("#{ForemanTemplates::Engine.root}/test/templates/core/filter1.erb", "a") do |filt|
-          filt << "Appended from another test \n"
-        end
-        post :import, params: { "repo" => "#{ForemanTemplates::Engine.root}/test/templates/core", "filter" => "filter1", "prefix" => '', "associate" => "new", "lock" => true, "force" => true }
+        import_params = {
+          :repo => "#{template_fixtures_path}/locking/core_initial",
+          :prefix => '',
+          :associate => "new",
+          :lock => true
+        }
+
+        post :import, params: import_params.merge(:repo => "#{template_fixtures_path}/locking/core_updated", :force => true)
         assert_response :success
         updated = JSON.parse(@response.body)['message']['templates']
         updated.each do |template|
-          if template['name'] == 'filter1 template'
-            puts template
-          end
+          assert template['imported']
         end
       end
 
       test "should associate template to taxonomies in metadata on import" do
-        Dir.mktmpdir do |tmpdir|
-          t_name = "metadata_with_taxonomies"
-          org = taxonomies(:organization1)
-          loc = taxonomies(:location1)
-          create_local_template t_folder: tmpdir, t_filename: "metadata", t_name: t_name, org: org.name, loc: loc.name
-          post :import, params: { "repo" => tmpdir, "filter" => t_name, "prefix" => '', "associate" => "always" }
-          assert_response :success
-          assert_equal ProvisioningTemplate.unscoped.find_by_name(t_name).organizations[0].name, org.name
-          assert_equal ProvisioningTemplate.unscoped.find_by_name(t_name).locations[0].name, loc.name
-        end
+        post :import, params: { "repo" => "#{template_fixtures_path}/with_taxonomies", "prefix" => '', "associate" => "always" }
+        assert_response :success
+        template = Ptable.unscoped.find_by :name => 'Test Ptable'
+        assert_equal 1, template.organizations.count
+        assert_equal template.organizations.first.name, @org.name
+        assert_equal 1, template.locations.count
+        assert_equal template.locations.first.name, @loc.name
       end
 
       test "should export filtered template only" do
@@ -123,24 +108,9 @@ module Api
         end
       end
 
-      test "should import accordingly to exported template metadata" do
-        org = taxonomies(:organization2)
-        loc = taxonomies(:location2)
-        name = "taxo_metadata_import"
-        templte = "<%#\nmodel: ProvisioningTemplate\nkind: provision\nname: #{name}\norganizations: #{org.name}\nlocations: #{loc.name}\n-%>\nimport metadata"
-        ProvisioningTemplate.create!(:name => name, :template => templte)
-        Dir.mktmpdir do |tmpdir|
-          post :export, params: { "repo" => tmpdir, "filter" => name, "metadata_export_mode" => "refresh"}
-          post :import, params: { "repo" => tmpdir, "filter" => name, "prefix" => "expMeta_", "associate" => "new" }
-          assert_response :success
-          assert_equal ProvisioningTemplate.unscoped.find_by_name(name).organizations[0].name, org.name
-          assert_equal ProvisioningTemplate.unscoped.find_by_name(name).locations[0].name, loc.name
-        end
-      end
-
       test "should export all the templates" do
         org = FactoryBot.create(:organization, :name => "org4")
-        for i in 1..5
+        (1..5).each do |i|
           FactoryBot.create(:ptable, :name => "all_ptable#{i}", :organizations => [org])
           FactoryBot.create(:provisioning_template, :name => "all_ptemplate#{i}", :organizations => [org])
         end
@@ -150,22 +120,6 @@ module Api
           templates = JSON.parse(@response.body)['message']['templates']
           assert_equal templates.size, 10
         end
-      end
-
-      private
-      def create_local_template(t_folder:, t_filename:, t_name:, model: "ProvisioningTemplate", kind: "provision", org: "", loc: "")
-        File.open("#{t_folder}/#{t_filename}.erb", "w") { |template|
-          template << "<%#\n"
-          template << "model: #{model}\n"
-          template << "kind: #{kind}\n"
-          template << "name: #{t_name}\n"
-          template << "oses:\n"
-          template << "- Debian 6.0\n"
-          template << "organizations: #{org}\n" if org
-          template << "locations: #{loc}\n" if loc
-          template << "-%>\n"
-          template << "testing #{t_name} #{model} .."
-        }
       end
     end
   end
---
+++ test/integration/foreman_templates_integration_test.rb
@@ -0,0 +1,30 @@
+require 'test_plugin_helper'
+
+class ForemanTemplatesIntegrationTest < ActionDispatch::IntegrationTest
+  test 'export and import should be idempotent when keeping metadata' do
+    name = 'Prefix Ptable'
+    admin = users(:apiadmin)
+    Dir.mktmpdir do |tmpdir|
+      common_params = { "repo" => "#{template_fixtures_path}/with_prefix", "prefix" => "Prefix" }
+      # rubocop:disable Rails/HttpPositionalArguments
+      post '/api/templates/import', **create_params(common_params.merge("associate" => "new"), admin, "secret")
+      assert_response :success
+      first_content = Template.find_by(:name => name).template
+      post '/api/templates/export', **create_params(common_params.merge("repo" => tmpdir, "metadata_export_mode" => "keep"), admin, "secret")
+      assert_response :success
+      post '/api/templates/import', **create_params(common_params.merge("repo" => tmpdir, "associate" => "new"), admin, "secret")
+      # rubocop:enable Rails/HttpPositionalArguments
+      assert_response :success
+      second_content = Template.find_by(:name => name).template
+      assert_equal first_content, second_content
+    end
+  end
+
+  def create_params(params, user, password)
+    {
+      params: params,
+      as: :json,
+      env: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(user.login, password) }
+    }
+  end
+end
