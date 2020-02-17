56b96c8f98c682bb69f1e7cc590d2c7ad788e815
Ondrej Prazak <oprazak@redhat.com>
2020-02-17 13:41:09 +0100

    Refs #24089 - Check if setting present in migration

--- db/migrate/20180627134929_change_lock_setting.rb
+++ db/migrate/20180627134929_change_lock_setting.rb
@@ -1,5 +1,5 @@
 class ChangeLockSetting < ActiveRecord::Migration[5.1]
   def up
-    Setting.find_by(:name => 'template_sync_lock').destroy
+    Setting.find_by(:name => 'template_sync_lock')&.destroy
   end
 end
