97e6165d56abf5fee58db284cba6edd26a7508b0
Ondrej Prazak <oprazak@redhat.com>
2020-02-26 16:20:54 +0100

    Fixes #29188 - Fix expandable content

---
+++ webpack/components/TemplateSyncResult/components/SyncedTemplate/helpers.test.js
@@ -0,0 +1,21 @@
+import { testSelectorsSnapshotWithFixtures } from 'react-redux-test-utils';
+
+import { expandableContent } from './helpers';
+
+const fixtures = {
+  'should return no errors': () => expandableContent({}),
+  'should return additional info': () =>
+    expandableContent({ additionalInfo: 'This is additional info' }),
+  'should return additional error': () =>
+    expandableContent({ additionalErrors: 'These are additional errors' }),
+  'should return attribute errors': () =>
+    expandableContent({
+      errors: {
+        base: ['could not be processed'],
+        name: ["can't be blank", 'has invalid format'],
+      },
+    }),
+};
+
+describe('expandableContent', () =>
+  testSelectorsSnapshotWithFixtures(fixtures));
