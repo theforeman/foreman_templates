f32540d1df188d9bc8e68453677bb42d4c0f8f53
Ondrej Prazak <oprazak@redhat.com>
2021-05-05 09:49:58 +0200

    Fix rubocop

--- test/integration/foreman_templates_integration_test.rb
+++ test/integration/foreman_templates_integration_test.rb
@@ -6,14 +6,12 @@ class ForemanTemplatesIntegrationTest < ActionDispatch::IntegrationTest
     admin = users(:apiadmin)
     Dir.mktmpdir do |tmpdir|
       common_params = { "repo" => "#{template_fixtures_path}/with_prefix", "prefix" => "Prefix", "lock" => "keep" }
-      # rubocop:disable Rails/HttpPositionalArguments
       post '/api/templates/import', **create_params(common_params.merge("associate" => "new"), admin, "secret")
       assert_response :success
       first_content = Template.find_by(:name => name).template
       post '/api/templates/export', **create_params(common_params.merge("repo" => tmpdir, "metadata_export_mode" => "keep"), admin, "secret")
       assert_response :success
       post '/api/templates/import', **create_params(common_params.merge("repo" => tmpdir, "associate" => "new"), admin, "secret")
-      # rubocop:enable Rails/HttpPositionalArguments
       assert_response :success
       second_content = Template.find_by(:name => name).template
       assert_equal first_content, second_content
