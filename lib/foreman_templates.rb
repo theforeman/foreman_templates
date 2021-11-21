b06085dc0dc5b4ac456125842f1cef29b6846c03
Ondřej Ezr <oezr@redhat.com>
2021-11-22 00:02:42 +0100

    Fixes #33962 - use definitions to load setting attributes
    This implements loading the setting attributes from SettingRegistry instead of loading from DB.

--- lib/foreman_templates.rb
+++ lib/foreman_templates.rb
@@ -1,4 +1,7 @@
 require 'foreman_templates/engine'
 
 module ForemanTemplates
+  BASE_SETTING_NAMES = %w(repo branch dirname filter negate).freeze
+  IMPORT_SETTING_NAMES = (BASE_SETTING_NAMES | %w(prefix associate force lock)).freeze
+  EXPORT_SETTING_NAMES = (BASE_SETTING_NAMES | %w(metadata_export_mode commit_msg)).freeze
 end
