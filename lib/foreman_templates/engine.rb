8896663e0489c7421f483c514e61fda493ca06d8
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-15 14:16:38 +0000

    Fixes #37468 - Support Zeitwerk loader

--- lib/foreman_templates/engine.rb
+++ lib/foreman_templates/engine.rb
@@ -24,7 +24,7 @@ module ForemanTemplates
 
     initializer 'foreman_templates.register_plugin', :before => :finisher_hook do
       Foreman::Plugin.register :foreman_templates do
-        requires_foreman '>= 3.7'
+        requires_foreman '>= 3.13'
         register_gettext
 
         apipie_documented_controllers ["#{ForemanTemplates::Engine.root}/app/controllers/api/v2/*.rb"]
