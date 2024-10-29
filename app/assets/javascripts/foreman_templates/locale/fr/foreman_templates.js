aff0e8e8a318cd7dbde6122a7b6813d21d5d96fc
Oleh Fedorenko <ofedoren@redhat.com>
2024-10-29 14:18:39 +0000

    i18n - pulling from tx

--- app/assets/javascripts/foreman_templates/locale/fr/foreman_templates.js
+++ app/assets/javascripts/foreman_templates/locale/fr/foreman_templates.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_templates": {
       "": {
-        "Project-Id-Version": "foreman_templates 10.0.0",
+        "Project-Id-Version": "foreman_templates 10.0.1",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2019-10-14 12:27+0000",
         "Last-Translator": "Ewoud Kohl van Wijngaarden <ewoud+transifex@kohlvanwijngaarden.nl>, 2024",
